b31d9ffe9277b4f39032091ed741375e363d4e79
Borys Boiko <burbokop@gmail.com>
2021-04-15 15:57:17 +0300

    config builder added

--- src/main/scala/org/burbokop/generators/ConfigureBuilder.scala
+++ src/main/scala/org/burbokop/generators/ConfigureBuilder.scala
@@ -5,10 +5,37 @@ import java.io.File
 
 object ConfigureBuilder {
 
+  case class Paths(bin: String, include: String, lib: String)
+  object Paths {
+    def fromCache(cache: List[RepositoryMetaData]) = {
+      cache
+        .map(_.currentVersion)
+        .filter(_.isDefined)
+        .map(_.get).map { version =>
+        if (version.builder == "configure") {
+          Paths(
+            s"${version.buildPath}${File.separator}bin",
+            s"${version.buildPath}${File.separator}include",
+            s"${version.buildPath}${File.separator}lib"
+          )
+        } else {
+          Paths("", "", "")
+        }
+      }
+    }
+  }
+
   def build(cache: List[RepositoryMetaData], inputPath: String, outputPath: String): Either[Throwable, Unit] = {
-    val activeVersions = cache.map(_.currentVersion).filter(_.isDefined).map(_.get)
-    println(s"build ($inputPath) activeVersions: $activeVersions")
-    println(s"->: ${activeVersions.map(_.buildPath + File.separator + "headers").mkString(":")}")
+    val paths = Paths.fromCache(cache)
+    println(s"build ($inputPath) paths: $paths")
+
+    def env = Seq(
+      "PATH" -> paths.map(_.bin).mkString(":"),
+      "CPATH" -> paths.map(_.include).mkString(":"),
+      "LD_LIBRARY_PATH" -> paths.map(_.lib).mkString(":")
+    )
+
+    println(s"-> env: $env")
 
     val configPath = s"$inputPath${File.separator}configure"
     if (new File(configPath).isFile) {
@@ -17,15 +44,12 @@ object ConfigureBuilder {
         outputFolder.mkdirs();
       }
       val inputFolder = new File(inputPath)
-      val permissionsChangeResult = sys.process.Process(Seq("chmod", "+x", "./configure"), inputFolder).!
-      val configResult = sys.process.Process(
-        Seq("./configure", s"--prefix=$outputPath"),
-        inputFolder,
-      ).!
+      val permissionsChangeResult = sys.process.Process(Seq("chmod", "+x", "./configure"), inputFolder, env:_*).!
+      val configResult = sys.process.Process(Seq("./configure", s"--prefix=$outputPath"), inputFolder, env:_*).!
       println(s"config result: $configResult (perm: $permissionsChangeResult)")
 
-      val r0 = sys.process.Process(Seq("make"), inputFolder).!
-      val r1 = sys.process.Process(Seq("make", "install"), inputFolder).!
+      val r0 = sys.process.Process(Seq("make"), inputFolder, env:_*).!
+      val r1 = sys.process.Process(Seq("make", "install"), inputFolder, env:_*).!
       if (r0 == 0 && r1 == 0) {
         Right()
       } else {
