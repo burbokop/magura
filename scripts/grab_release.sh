038d11c474ef3545127cccd8b416bf0a4f6fbab7
Borys Boiko <burbokop@gmail.com>
2021-04-29 21:16:41 +0300

    test commit

--- scripts/grab_release.sh
+++ scripts/grab_release.sh
@@ -5,3 +5,11 @@ RELEASE_INFO_PATH=$SCRIPT_DIR/../target/release.info
 export MAGURA_TAG=$(awk -F "=" '/tag_name/ {print $2}' $RELEASE_INFO_PATH)
 export MAGURA_NEED_RELEASE=$(awk -F "=" '/need_release/ {print $2}' $RELEASE_INFO_PATH)
 export MAGURA_DEB=$(awk -F "=" '/deb/ {print $2}' $RELEASE_INFO_PATH)
+
+echo "RELEASE_INFO_PATH: $RELEASE_INFO_PATH"
+
+cat $RELEASE_INFO_PATH
+
+echo "MAGURA_TAG: $MAGURA_TAG"
+echo "MAGURA_NEED_RELEASE: $MAGURA_NEED_RELEASE"
+echo "MAGURA_DEB: $MAGURA_DEB"
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR/..
+
 sbt assembly
 sbt debian:packageBin
 source ./scripts/grab_release.sh
