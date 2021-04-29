69814df1ede873ca166d695ab1bb981c0ed03887
Borys Boiko <burbokop@gmail.com>
2021-04-29 21:37:59 +0300

    +

--- scripts/grab_release.sh
+++ scripts/grab_release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 RELEASE_INFO_PATH=$SCRIPT_DIR/../target/release.info
-export MAGURA_TAG=$(awk -F "=" '/tag_name/ {print $2}' $RELEASE_INFO_PATH)
-export MAGURA_NEED_RELEASE=$(awk -F "=" '/need_release/ {print $2}' $RELEASE_INFO_PATH)
-export MAGURA_DEB=$(awk -F "=" '/deb/ {print $2}' $RELEASE_INFO_PATH)
+export MAGURA_TAG=${$(awk -F "=" '/tag_name/ {print $2}' $RELEASE_INFO_PATH)##*( )}
+export MAGURA_NEED_RELEASE=${$(awk -F "=" '/need_release/ {print $2}' $RELEASE_INFO_PATH)##*( )}
+export MAGURA_DEB=${$(awk -F "=" '/deb/ {print $2}' $RELEASE_INFO_PATH)##*( )}
 
 echo "RELEASE_INFO_PATH: $RELEASE_INFO_PATH"
 
