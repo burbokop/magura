994d19414b888bc6e54590fa10ebca9ad80269d6
Borys Boiko <burbokop@gmail.com>
2021-04-29 21:43:32 +0300

    trim added

--- scripts/grab_release.sh
+++ scripts/grab_release.sh
@@ -3,14 +3,18 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 RELEASE_INFO_PATH=$SCRIPT_DIR/../target/release.info
-export MAGURA_TAG=${$(awk -F "=" '/tag_name/ {print $2}' $RELEASE_INFO_PATH)##*( )}
-export MAGURA_NEED_RELEASE=${$(awk -F "=" '/need_release/ {print $2}' $RELEASE_INFO_PATH)##*( )}
-export MAGURA_DEB=${$(awk -F "=" '/deb/ {print $2}' $RELEASE_INFO_PATH)##*( )}
+export MAGURA_TAG=$(awk -F "=" '/tag_name/ {print $2}' $RELEASE_INFO_PATH)
+export MAGURA_NEED_RELEASE=$(awk -F "=" '/need_release/ {print $2}' $RELEASE_INFO_PATH)
+export MAGURA_DEB=$(awk -F "=" '/deb/ {print $2}' $RELEASE_INFO_PATH)
+
+export MAGURA_TAG=`echo $MAGURA_TAG | sed 's/ *$//g'`
+export MAGURA_NEED_RELEASE=`echo $MAGURA_NEED_RELEASE | sed 's/ *$//g'`
+export MAGURA_DEB=`echo $MAGURA_DEB | sed 's/ *$//g'`
 
 echo "RELEASE_INFO_PATH: $RELEASE_INFO_PATH"
 
 cat $RELEASE_INFO_PATH
 
-echo "MAGURA_TAG: $MAGURA_TAG"
+echo "MAGURA_TAG: '$MAGURA_TAG'"
 echo "MAGURA_NEED_RELEASE: $MAGURA_NEED_RELEASE"
 echo "MAGURA_DEB: $MAGURA_DEB"
