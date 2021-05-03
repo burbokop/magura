78f34db5f6b90e6224e234e0c4b683a987a8a48b
Borys Boiko <burbokop@gmail.com>
2021-05-03 12:36:41 +0300

    install script changed

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 
 
 # installation
-In repo root:
-1. `sbt assembly`
-1. `sbt debian:packageBin`
-2. `sudo dpkg -i ./target/magura_0.1.2_all.deb`
-
-or run `./install.sh`
+In repo root: `./scripts/install.sh`
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+CURRENT_DIR=`pwd`
 cd $SCRIPT_DIR/..
 
 sbt assembly
 sbt debian:packageBin
 source ./scripts/grab_release.sh
 sudo dpkg -i $MAGURA_DEB
+
+cd $CURRENT_DIR
